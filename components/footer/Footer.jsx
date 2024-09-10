c2ae1de6d929b7f7728222d9a735f82988982369
BungZkert <121861527+BungZkert@users.noreply.github.com>
2024-09-11 00:11:53 +0700

    Add files via upload

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./footer.css";
+import { AiFillFacebook, AiFillTwitterSquare, AiFillInstagram} from "react-icons/ai";
+
+
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-content">
+            <p>
+                &copy; 2024 Your E-Learning Phatporm. All rights reserved. <br />
+                Made with 💖 <a href="">Prashant Kumar</a>
+            </p>
+            <div className="social-links">
+                <a href=""><AiFillFacebook/>
+                </a>
+                <a href=""><AiFillTwitterSquare/></a>
+                <a href=""><AiFillInstagram/></a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
